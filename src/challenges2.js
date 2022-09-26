b9c6d6a15ed48acca3699eab72e086c7e41b6668
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-09-26 13:17:55 -0300

    Initial commit

---
+++ src/challenges2.js
@@ -0,0 +1,20 @@
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+module.exports = {
+  generatePhoneNumber,
+  hydrate,
+  triangleCheck,
+};
