9db59acd24b3801acb23fcd387b998e150e0ee97
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-09-27 15:41:37 -0300

    compareTue ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(param1,param2) {
+
+  if (param1===true&&param2===true) {
+    return true;
+  }
+  return false;
+
+}   
+
 
 // Desafio 2
 function calcArea() {
