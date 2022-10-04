db2b03a6cfad9480543c1211b538d83b24068dd9
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-03 23:30:07 -0300

    hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -58,8 +58,20 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(bebida) {
+
+  let numbers = bebida.match(/\d+/g).map(Number);
+
+  let soma = 0;
+  for (let index = 0; index < numbers.length; index +=1) {
+    soma +=numbers[index];
+    
+  }
+  if ( soma !==1){
+    return `${soma} copos de água`;
+    }
+    return `${soma} copo de água`;
+
 }
 
 module.exports = {
