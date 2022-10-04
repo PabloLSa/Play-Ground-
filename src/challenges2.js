c9f3ad1325ee725e21d4e8add709208c8e4f37bf
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-03 22:59:44 -0300

    phoneNUmber

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,46 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+
+if (numbers.length!==11) {
+
+  return 'Array com tamanho incorreto.' ;
 }
+
+for (let index of numbers) {
+
+  if(numbers[index]<0||numbers[index]>9 ){
+
+    return  'não é possível gerar um número de telefone com esses valores' ;
+  }
+}
+  let counts = {};
+
+  numbers.forEach((count) => {
+
+    counts[count] = (counts[count]||0)+1;
+
+    
+  });
+
+  const maxValue =  Math.max(...Object.values(counts));
+
+    if (maxValue >=3) {
+
+      return 'não é possível gerar um número de telefone com esses valores';
+      
+    }
+
+    let phone = numbers.join('');
+    let ddd = phone.substring(0,2);
+    let phone1 = phone.substring(2,7);
+    let phone2 = phone.substring(7,11);
+
+    return '('+ddd +') ' + phone1 + "-"+phone2;
+  
+}
+  
+
+
 
 // Desafio 12
 function triangleCheck() {
